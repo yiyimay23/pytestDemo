fcd146274897e1ed47bf151d51960c1b3dce3777
yiyimay23 <1225622868@qq.com>
2025-04-16 10:23:34 +0800

    test
    更新报告路径，pytest.ini

--- run.py
+++ run.py
@@ -11,6 +11,6 @@ import time
 if __name__ == "__main__":
     pytest.main(["-s", "./", "--capture=sys"])  # --capture=sys会把报错的情况写进测试报告中
     # 如果每次都覆盖前一个报告，可以不添加时间戳
-    os.system('allure generate ./report -o ./report/allure_html --clean')
+    os.system('allure generate ./report/result -o ./report/allure_html --clean')
     # 保留每个报告记录，报告加时间戳
-    # os.system('allure generate ./report -o ./report/{}.html --clean'.format(time.strftime("%Y%m%d-%H%M%S")))
+    # os.system('allure generate ./report/result -o ./report/{}.html --clean'.format(time.strftime("%Y%m%d-%H%M%S")))
